38dfc8f6315b1da047ebab2845ad05419772e060
AHausken <alexander.hausken@gmail.com>
2021-02-12 19:48:01 +0100

    added TeamNow comp

---
+++ arbeidskrav/src/components/fotball/TeamNowList.vue
@@ -0,0 +1,39 @@
+<template>
+    <TeamNowItem
+
+    v-for="(teamnow, index) in teamToday"
+    :key="index"
+    :player="teamnow.player"
+    :year="teamnow.year"
+    :position="teamnow.position"
+/>
+    
+</template>
+
+<script>
+import { ref } from 'vue'
+import TeamNowItem from './TeamNowItem' 
+
+export default {
+    setup(){
+    
+    const teamToday = ref([
+        {player: "David de Gea", year: "2011 - D.d", position: "Keeper"},
+        {player: "Aaron Wan-Bissaka", year: "2019 - D.d", position: "Right Back"},
+        {player: "Victor Lindelöf", year: "2017 - D.d", position: "Center Back"},
+        {player: "Harry Maguire", year: "2019 - D.d", position: "Center Back"},
+        {player: "Luke Shaw", year: "2014 - D.d", position: "Left Back"},
+        {player: "Nemanja Matić", year: "2017 - D.d", position: "Midfield"},
+        {player: "Frederico Rodrigues Santos", year: "2017 - D.d", position: "Midfield"},
+        {player: "Paul Pogba", year: "2016 - D.d", position: "Midfield"},
+        {player: "Bruno Fernandes", year: "2020 - D.d", position: "Midfield"},
+        {player: "Marcus Rashford", year: "2005 - D.d", position: "Forward"},
+        {player: "Edinson Cavani", year: "2020 - D.d", position: "Forward"},
+    ]);
+
+    return{teamToday, TeamNowItem}
+
+    }
+
+}
+</script>
--- arbeidskrav/src/views/Lag.vue
+++ arbeidskrav/src/views/Lag.vue
@@ -2,13 +2,22 @@
     <h1>All time favoritt lag VS laget i 2021</h1>
     <br>
 
-    <input type="button" @click="showFav =! showFav" value="Trykk for favoritt lag">
+    <input type="button" @click="showFav =! showFav" value="Trykk for mitt favoritt lag">
+    
+    <input type="button" @click="showTeamNow =! showTeamNow" value="Trykk for å se hvordan laget ser ut i dag">
 
     
 
     <section v-if="showFav">
        <h2>All Time Favs</h2> 
       <FavTeamList/>  
+      <br>
+    </section>
+
+    <section v-if="showTeamNow">
+        <br>
+        <h2>Team now</h2>
+        <TeamNowList/>
     </section>
     
 
@@ -16,21 +25,19 @@
 
 <script>
 import FavTeamList from '../components/fotball/FavTeamList'
+import TeamNowList from '../components/fotball/TeamNowList'
 export default {
-    components: {FavTeamList},
+    components: {FavTeamList, TeamNowList},
     setup(){
-        return {FavTeamList}
+        return {FavTeamList, TeamNowList }
     },
 
-    data:() =>{
+    data:() => {
         return{
-            showFav: false
+            showFav: false,
+            showTeamNow: false
         }
     }
     
 }
 </script>
-
-<style scoped>
-
-</style>
